b1882a14471955d4fd6b90ea05bc6719ddc474a7
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2023-08-12 17:34:43 -0300

    feat: create temp pages

---
+++ frontend/src/pages/OptionsPage.js
@@ -0,0 +1,7 @@
+export default function OptionsPage() {
+  return (
+    <div>
+      <p>OptionsPage works!</p>
+    </div>
+  );
+}
---
+++ frontend/src/pages/ProfilePage.js
@@ -0,0 +1,7 @@
+export default function ProfilePage() {
+  return (
+    <div>
+      <p>ProfilePage works!</p>
+    </div>
+  );
+}
