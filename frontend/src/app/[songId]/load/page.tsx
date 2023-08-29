1736c3f3da2f3bca3203422eb2f82073d2d86ab6
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2023-08-29 17:46:19 -0300

    refactor: move play page to same level as load

--- frontend/src/app/[songId]/load/page.tsx
+++ frontend/src/app/[songId]/load/page.tsx
@@ -1,36 +1,32 @@
-"use client"
+"use client";
 import "./Load.scss";
 import { useParams } from "next/navigation";
 import { serverURL } from "@/config";
 import Image from "next/image";
 
+const fetchAndRedirect = async (
+  songName: string,
+  instrument: string,
+  destination: string
+) => {
+  const response = await fetch(
+    `${serverURL}/track-with-removed-stem?filename=${songName}.mp3&stem_to_remove=${instrument}`
+  );
 
-const fetchAndRedirect = async (songName: string, instrument: string, destination: string) => {
-    const response = await fetch(
-        `${serverURL}/track-with-removed-stem?filename=${songName}.mp3&stem_to_remove=${instrument}`
-    );
+  if (response.ok) {
+    window.location.href = destination;
+  }
+};
 
-    if (response.ok) {
-        window.location.href = destination;
-    }
-}
+export default function Load() {
+  const params = useParams();
+  const songId = params.songId;
 
-export default function Load(){
-    const params = useParams();
-    const songId = params.songId;
+  const songName = "Riots";
+  const instrument = "vocals";
+  const destination = `/${songId}/play`;
 
-    const songName = "Riots";
-    const instrument = "vocals";
-    const destination = `/${songId}/load/play`
+  fetchAndRedirect(songName, instrument, destination);
 
-    fetchAndRedirect(songName, instrument, destination);
-
-    return (
-        <Image
-        src={"/gifs/loading.gif"}
-        alt="loading gif"
-        width={100}
-        height={100}
-        />
-    )
+  return <Image src={"/gifs/loading.gif"} alt="loading gif" width={100} height={100} />;
 }
