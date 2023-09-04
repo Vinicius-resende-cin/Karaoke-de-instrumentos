2d8e8ed5ab3492c7540bd5ec10a580ee52147660
Pedro Flor <66924915+peu-savio@users.noreply.github.com>
2023-09-04 16:09:27 -0300

    Update searchVideos.py

--- backend/src/usecase/searchVideos.py
+++ backend/src/usecase/searchVideos.py
@@ -4,13 +4,5 @@ class YoutubeSearchUseCase:
     def __init__(self, youtube_search_repository: YoutubeSearchRepository) -> None:
         self.youtube_search_repository = youtube_search_repository
 
-    def execute(self, query: str, num_results: int = 10, result_type: str = 'videos') -> dict:
-        try:
-            results = self.youtube_search_repository.search_videos(query, num_results, result_type)
-            return results
-        except ConnectionError as e:
-            print(f"Um erro de conexão aconteceu: {e}")
-            return {}
-        except Exception as e:
-            print(f"Um erro aconteceu: {e}")
-            return {}
+    def search_videos(self, query: str, num_results: int = 10, result_type: str = 'videos') -> dict:
+        return self.youtube_search_repository.search_videos(query, num_results, result_type)
