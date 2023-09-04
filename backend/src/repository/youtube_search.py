059c3f6501ca01c25d9ec8b10ae56f66fe2b8fd9
Pedro Flor <66924915+peu-savio@users.noreply.github.com>
2023-09-04 15:41:32 -0300

    Update youtube_search.py

--- backend/src/repository/youtube_search.py
+++ backend/src/repository/youtube_search.py
@@ -1,20 +1,21 @@
 from youtubesearchpython import VideosSearch
 
-def youtube_search(query: str, num_results: int = 10, result_type: str = 'videos') -> dict:
-    try:
-        videosSearch = VideosSearch(query, limit=num_results, type=result_type)
-        results = {}
-        if videosSearch.result()['result']:
-            for i in range(len(videosSearch.result()['result'])):
-                title = videosSearch.result()['result'][i]['title']
-                url = videosSearch.result()['result'][i]['link']
-                results[title] = url
-        else:
-            print("Nenhum resultado encontrado.")
-        return results
-    except ConnectionError as e:
-        print(f"Um erro de conexão aconteceu: {e}")
-        return {}
-    except Exception as e:
-        print(f"Um erro aconteceu: {e}")
-        return {}
+class YoutubeSearch:
+    def search_videos(self, query: str, num_results: int = 10, result_type: str = 'videos') -> dict:
+        try:
+            videosSearch = VideosSearch(query, limit=num_results, type=result_type)
+            results = {}
+            if videosSearch.result()['result']:
+                for i in range(len(videosSearch.result()['result'])):
+                    title = videosSearch.result()['result'][i]['title']
+                    url = videosSearch.result()['result'][i]['link']
+                    results[title] = url
+            else:
+                print("Nenhum resultado encontrado.")
+            return results
+        except ConnectionError as e:
+            print(f"Um erro de conexão aconteceu: {e}")
+            return {}
+        except Exception as e:
+            print(f"Um erro aconteceu: {e}")
+            return {}
