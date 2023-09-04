4e1d10863c242feeb9757537cfb01568aabe8207
Pedro Flor <66924915+peu-savio@users.noreply.github.com>
2023-09-04 16:10:10 -0300

    Update searchVideos.py

--- backend/src/usecase/searchVideos.py
+++ backend/src/usecase/searchVideos.py
@@ -4,5 +4,5 @@ class YoutubeSearchUseCase:
     def __init__(self, youtube_search_repository: YoutubeSearchRepository) -> None:
         self.youtube_search_repository = youtube_search_repository
 
-    def search_videos(self, query: str, num_results: int = 10, result_type: str = 'videos') -> dict:
+    def execute(self, query: str, num_results: int = 10, result_type: str = 'videos') -> dict:
         return self.youtube_search_repository.search_videos(query, num_results, result_type)
