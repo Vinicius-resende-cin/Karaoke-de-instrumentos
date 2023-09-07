40416cae27d03165410f1ab485368e874db57d90
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2023-09-07 07:43:33 -0300

    feat: add starred songs list

--- frontend/src/app/(navBar)/home/page.tsx
+++ frontend/src/app/(navBar)/home/page.tsx
@@ -3,16 +3,29 @@ import TopSongs from "./api/data.json";
 import SongList from "@/components/SongList/Songlist";
 
 export default function HomePage() {
-  const top10_songs = TopSongs["songs"];
+  const top10Songs = TopSongs["songs"];
+  const starredSongs = [
+    {
+      id: "AtVZifGCX7s",
+      title: "Stuck in the Sound - Riots [official video]",
+      thumb: {
+        url: "https://i.ytimg.com/vi/AtVZifGCX7s/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC2gKIfmS6oexJAs7qxZrbfbWwRBg",
+        width: 720,
+        height: 404
+      },
+      url: "https://www.youtube.com/watch?v=AtVZifGCX7s"
+    }
+  ];
 
   return (
     <div className="p-10 flex flex-col items-center justify-start gap-10">
       <div className="flex flex-col items-center justify-center gap-10">
+        <h1 className="font-bold text-3xl">Favoritos</h1>
+        <SongList songs={starredSongs} />
+      </div>
+      <div className="flex flex-col items-center justify-center gap-10">
         <h1 className="font-bold text-3xl">Populares</h1>
-        <Link href="/AtVZifGCX7s" className="text-xl">
-          Riots - Stuck in the Sound *remover
-        </Link>
-        <SongList songs={top10_songs} />
+        <SongList songs={top10Songs} />
       </div>
     </div>
   );
