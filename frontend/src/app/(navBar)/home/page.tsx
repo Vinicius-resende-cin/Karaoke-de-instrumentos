35ef2f25e377a907d9ed4909421bec874dc69e66
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2023-09-06 22:13:06 -0300

    feat: add song list

--- frontend/src/app/(navBar)/home/page.tsx
+++ frontend/src/app/(navBar)/home/page.tsx
@@ -1,16 +1,18 @@
 import Link from "next/link";
+import TopSongs from "./api/data.json";
+import SongList from "@/components/SongList/Songlist";
 
 export default function HomePage() {
+  const top10_songs = TopSongs["songs"];
+
   return (
     <div className="p-10 flex flex-col items-center justify-start gap-10">
-      <div className="flex flex-col items-center justify-center gap-5">
+      <div className="flex flex-col items-center justify-center gap-10">
         <h1 className="font-bold text-3xl">Populares</h1>
         <Link href="/AtVZifGCX7s" className="text-xl">
-          Riots - Stuck in the Sound
+          Riots - Stuck in the Sound *remover
         </Link>
-      </div>
-      <div>
-        <h1 className="font-bold text-3xl">Sugestões</h1>
+        <SongList songs={top10_songs} />
       </div>
     </div>
   );
