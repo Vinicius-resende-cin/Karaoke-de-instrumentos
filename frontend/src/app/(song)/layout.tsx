80073ae8693bd49c0503d5788cc1a939c5b2bcf7
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2023-09-09 04:52:29 -0300

    feat(style): adjust styles to only two pages

--- frontend/src/app/(song)/layout.tsx
+++ frontend/src/app/(song)/layout.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import { InstrumentProvider } from "./contexts/IntrumentContext";
+
+export const metadata: Metadata = {
+  title: "Karaoke de Instrumentos",
+  description: "App de karaoke de instrumentos"
+};
 
 export default function SongPagesLayout({ children }: { children: React.ReactNode }) {
   return (
--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import Image from "next/image";
 import NavButton from "@/components/NavBar/NavButton/NavButton";
 
 export default function NavBar() {
@@ -11,43 +12,37 @@ export default function NavBar() {
 
   return (
     <nav>
-      <div className="flex flex-row justify-around p-6">
-        <NavButton
-          link="/home"
-          name="home"
-          image="/icons/home.ico"
-          image_selected="/icons/home_selected.ico"
-          size={50}
-          selected={selectedButton === "home"}
-          handleClick={handleButtonClick}
-        />
-        <NavButton
-          link="/playlist"
-          name="playlist"
-          image="/icons/song.ico"
-          image_selected="/icons/song_selected.ico"
-          size={50}
-          selected={selectedButton === "playlist"}
-          handleClick={handleButtonClick}
-        />
-        <NavButton
-          link="/profile"
-          name="profile"
-          image="/icons/profile.ico"
-          image_selected="/icons/profile_selected.ico"
-          size={50}
-          selected={selectedButton === "profile"}
-          handleClick={handleButtonClick}
-        />
-        <NavButton
-          link="/options"
-          name="options"
-          image="/icons/options.ico"
-          image_selected="/icons/options_selected.ico"
-          size={50}
-          selected={selectedButton === "options"}
-          handleClick={handleButtonClick}
-        />
+      <div className="flex flex-row justify-between px-5 py-6">
+        <span className="flex items-center gap-2 text-xl font-bold">
+          <Image
+            src="/icons/mic.ico"
+            alt="Logo"
+            className="dark:invert"
+            width={50}
+            height={50}
+          />
+          Karaoke
+        </span>
+        <span className="flex items-center gap-5">
+          <NavButton
+            link="/home"
+            name="home"
+            image="/icons/home.ico"
+            image_selected="/icons/home_selected.ico"
+            size={50}
+            selected={selectedButton === "home"}
+            handleClick={handleButtonClick}
+          />
+          <NavButton
+            link="/playlist"
+            name="playlist"
+            image="/icons/song.ico"
+            image_selected="/icons/song_selected.ico"
+            size={50}
+            selected={selectedButton === "playlist"}
+            handleClick={handleButtonClick}
+          />
+        </span>
       </div>
     </nav>
   );
