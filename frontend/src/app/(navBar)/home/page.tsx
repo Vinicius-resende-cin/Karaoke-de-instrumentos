af705a0b88f2f7b3c0732bc9c76449aa94aac893
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2023-09-16 15:39:45 -0300

    feat: add playlist page

--- frontend/src/app/(navBar)/home/page.tsx
+++ frontend/src/app/(navBar)/home/page.tsx
@@ -1,28 +1,11 @@
-import Link from "next/link";
-import TopSongs from "./api/data.json";
+import Songs from "../api/songs.json";
 import SongList from "@/components/SongList/Songlist";
 
 export default function HomePage() {
-  const top10Songs = TopSongs["songs"];
-  const starredSongs = [
-    {
-      id: "AtVZifGCX7s",
-      title: "Stuck in the Sound - Riots [official video]",
-      thumb: {
-        url: "https://i.ytimg.com/vi/AtVZifGCX7s/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC2gKIfmS6oexJAs7qxZrbfbWwRBg",
-        width: 720,
-        height: 404
-      },
-      url: "https://www.youtube.com/watch?v=AtVZifGCX7s"
-    }
-  ];
+  const top10Songs = Songs["top"];
 
   return (
     <div className="p-10 flex flex-col items-center justify-start gap-10">
-      <div className="flex flex-col items-center justify-center gap-10">
-        <h1 className="font-bold text-3xl">Favoritos</h1>
-        <SongList songs={starredSongs} />
-      </div>
       <div className="flex flex-col items-center justify-center gap-10">
         <h1 className="font-bold text-3xl">Populares</h1>
         <SongList songs={top10Songs} />
